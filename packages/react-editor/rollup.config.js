4cd734b04ad0518a1003871be21f2e0d0779d2c9
Fritz Lin <uxfritz@163.com>
2020-02-23 22:16:07 +0800

    chore: bump dependencies with lib rollup

--- packages/react-editor/rollup.config.js
+++ packages/react-editor/rollup.config.js
@@ -1,11 +1,9 @@
-import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
 // import postcss from 'rollup-plugin-postcss-modules'
 import postcss from 'rollup-plugin-postcss';
-import resolve from 'rollup-plugin-node-resolve';
-import url from 'rollup-plugin-url';
-import svgr from '@svgr/rollup';
+import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
 export default {
@@ -29,8 +27,6 @@ export default {
     postcss({
       modules: true,
     }),
-    url(),
-    svgr(),
     resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
